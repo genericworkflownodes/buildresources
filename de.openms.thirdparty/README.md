4aab2b5c39829fd6cc178f67ac3b23842eae0e75
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2023-03-01 19:40:44 +0100

    added resources for handling the OpenMS plugin split

---
+++ de.openms.thirdparty/README.md
@@ -0,0 +1,4 @@
+This folder provides resources to be added to the **built** thirdparty plugin to make the old nodes still usable
+after the split from the main plugin.
+Copy the java class to the same subfolder in which it lies. Add the plugin.xml.snippet as a last <extension>
+to the plugin.xml.
---
+++ de.openms.thirdparty/src/de/openms/thirdparty/knime/OpenMSNodeFactoryClassMapper.java
@@ -0,0 +1,40 @@
+package de.openms.thirdparty.knime;
+
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.knime.core.node.MapNodeFactoryClassMapper;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeModel;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+public class OpenMSNodeFactoryClassMapper extends MapNodeFactoryClassMapper {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected Map<String, Class<? extends NodeFactory<? extends NodeModel>>> getMapInternal() {
+        Map<String, Class<? extends NodeFactory<? extends NodeModel>>> map = new HashMap<>();
+        Bundle bundle = FrameworkUtil.getBundle(getClass());
+        Enumeration<URL> entries = bundle.findEntries("/target/classes/de/openms/thirdparty/knime/nodes/", "*Factory.class", true);
+        while (entries.hasMoreElements())
+        {
+            URL classURL = entries.nextElement();
+            String path = classURL.getFile();
+            String classnameNoBinFolderDots = path.replace("/target/classes/", "").replace(".class", "").replace('/', '.');
+            String oldClassname = classnameNoBinFolderDots.replace("thirdparty.", "");
+            
+            Class<? extends NodeFactory<? extends NodeModel>> c;
+            try {
+                c = (Class<? extends NodeFactory<? extends NodeModel>>) bundle.loadClass(classnameNoBinFolderDots);
+                map.put(oldClassname, c);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return map;
+    }
+
+}
